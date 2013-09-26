433c8ea1d270ee409249e6066b4ffb84566e186f
maximegengler <Biffi@MacBook-Pro-de-Maxime.local>
2013-09-26 14:36:02 +0200

    divers modifications de style

--- themes/storm/single.php
+++ themes/storm/single.php
@@ -23,8 +23,8 @@
 				<!-- /post title -->
 				
 				<!-- post details -->
-				<span class="date"><?php the_time('F j, Y'); ?> <?php the_time('g:i a'); ?></span>
-				<span class="author"><?php _e( 'Published by', 'html5blank' ); ?> <?php the_author_posts_link(); ?></span>
+				<span class="date">Le <?php the_time('d/m/Y'); ?>, </span>
+				<span class="author">par <?php the_author_posts_link(); ?></span>
 				<span class="comments"><?php comments_popup_link( __( 'Leave your thoughts', 'html5blank' ), __( '1 Comment', 'html5blank' ), __( '% Comments', 'html5blank' )); ?></span>
 				<!-- /post details -->
 				
@@ -32,9 +32,9 @@
 				
 				<?php the_tags( __( 'Tags: ', 'html5blank' ), ', ', '<br>'); // Separated by commas with a line break at the end ?>
 				
-				<p><?php _e( 'Categorised in: ', 'html5blank' ); the_category(', '); // Separated by commas ?></p>
+				<!-- <p><?php _e( 'Categorised in: ', 'html5blank' ); the_category(', '); // Separated by commas ?></p> -->
 				
-				<p><?php _e( 'This post was written by ', 'html5blank' ); the_author(); ?></p>
+				<!-- <p><?php _e( 'This post was written by ', 'html5blank' ); the_author(); ?></p> -->
 				
 				<?php edit_post_link(); // Always handy to have Edit Post Links available ?>
 				
