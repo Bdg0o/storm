af0fc34ef25c40c4869acacaaa657e8c72dd5668
Bdg <royeran@gmail.com>
2013-10-07 20:20:55 +0200

    Correction bugs

--- themes/storm/functions.php
+++ themes/storm/functions.php
@@ -540,39 +540,6 @@ function the_breadcrumbs() {
     elseif (is_search()) {echo"Search results: "; }
 }
 
-/* FORMULAIRE D'INSCRIPTION */
-
-    //1. Add a new form element...
-    add_action('register_form','storm_register_form');
-    function storm_register_form (){
-        $first_name = ( isset( $_POST['first_name'] ) ) ? $_POST['first_name']: '';
-        ?>
-        <p>
-            <label for="first_name"><?php _e('First Name','mydomain') ?><br />
-                <input type="text" name="first_name" id="first_name" class="input" value="<?php echo esc_attr(stripslashes($first_name)); ?>" size="25" /></label>
-        </p>
-        <?php
-    }
-
-    //2. Add validation. In this case, we make sure first_name is required.
-    add_filter('registration_errors', 'storm_registration_errors', 10, 3);
-    function storm_registration_errors ($errors, $sanitized_user_login, $user_email) {
-
-        if ( empty( $_POST['first_name'] ) )
-            $errors->add( 'first_name_error', __('<strong>ERROR</strong>: You must include a first name.','mydomain') );
-
-        return $errors;
-    }
-
-    //3. Finally, save our extra registration user meta.
-    add_action('user_register', 'storm_user_register');
-    function storm_user_register ($user_id) {
-        if ( isset( $_POST['first_name'] ) )
-            update_user_meta($user_id, 'first_name', $_POST['first_name']);
-    }
-
-
-
 /* Ajout de Maxime */
 
 	// Custom du preview des articles
